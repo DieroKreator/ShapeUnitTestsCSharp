faf73a10a320c930267a2cdb5663bcdb8409891f
DieroKreator <qtechwired@gmail.com>
2025-03-24 17:30:53 -0300

    Add unit tests for ShapesAreaCalculator

---
+++ ShapesUnitTestCSharp/ShapesAreaCalculatorTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using static AreaLibrary.ShapesAreaCalculator;
+
+namespace ShapesUnitTestCSharp;
+
+[TestFixture]
+public class ShapesAreaCalculatorTests
+{
+    [Test]
+    public void CalculateArea_ValidSide_ReturnsCorrectArea()
+    {
+        double side = 3;
+        double expected = 54;
+        double actual = Cube.CalculateArea(side);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void CalculateArea_NegativeSide_ThrowsArgumentException()
+    {
+        double side = -2;
+        Assert.Throws<ArgumentException>(() => Cube.CalculateArea(side));
+    }
+}
